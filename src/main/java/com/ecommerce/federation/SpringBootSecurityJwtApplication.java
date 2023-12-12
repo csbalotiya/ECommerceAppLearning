de515efa3f8bb4156f99f54a675bce67e5464056
csbalotiya <csbalotiya@gmail.com>
2023-12-12 23:58:18 +0530

    [MasterCS]: Added federation functionality

---
+++ src/main/java/com/ecommerce/federation/SpringBootSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.federation;
+
+import org.springframework.boot.SpringApplication;
+
+//@Configuration
+public class SpringBootSecurityJwtApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+    }
+
+}
